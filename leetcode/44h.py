a414378bd2e7e59655e8e77930bd549d4f34e077
Vladimir Kuznetsov <v.j.kuznetsov@gmail.com>
2021-02-07 21:26:22 +0400

    Дорешал leetcode 44: wildcard matching (hard)

--- leetcode/44h.py
+++ leetcode/44h.py
@@ -2,39 +2,51 @@
 # leetcode 44
 # hard
 
-# NOT ACCEPTED
-# TIME LIMIT EXCEEDED
+# ACCEPTED
+
 
 import sys
 
 
 def solution(s, p):
+    # без мемоизации не проходит timelimit
+    mem = [[-1 for _ in range(len(p)+1)] for _ in range(len(s)+1)]
 
     def check(pos_w, pos_p):
         nonlocal max_w, max_p, s, p 
+        if mem[pos_w][pos_p] != -1:
+            return mem[pos_w][pos_p]
+
         if pos_p == max_p:
+            mem[pos_w][pos_p] = pos_w == max_w
             return pos_w == max_w
         
         if p[pos_p] == '*':
             if pos_w == max_w:
-                return check(pos_w, pos_p + 1)
+                mem[pos_w][pos_p] = check(pos_w, pos_p + 1)
+                return mem[pos_w][pos_p]
             else:
-                return check(pos_w, pos_p + 1) or check(pos_w + 1, pos_p + 1) or check(pos_w + 1, pos_p)
+                mem[pos_w][pos_p] = check(pos_w, pos_p + 1) or check(pos_w + 1, pos_p + 1) or check(pos_w + 1, pos_p)
+                return mem[pos_w][pos_p]
     
         else:
             if pos_w == max_w:
+                mem[pos_w][pos_p] = False
                 return False
             
             if p[pos_p] == '?':
-                return check(pos_w + 1, pos_p + 1)
+                mem[pos_w][pos_p] = check(pos_w + 1, pos_p + 1)
+                return mem[pos_w][pos_p]
             
 
             if p[pos_p] != s[pos_w]:
+                mem[pos_w][pos_p] = False
                 return False
             
-            return check(pos_w + 1, pos_p + 1)
+            mem[pos_w][pos_p] = check(pos_w + 1, pos_p + 1)
+            return mem[pos_w][pos_p]
 
-    sys.setrecursionlimit(10**6)
+    # возможно лишняя оптимизация с несколькими "*" подряд
     if len(p) != 0:
         np = p[0]
         for i in range(1, len(p)):
@@ -42,6 +54,8 @@ def solution(s, p):
                 continue
             else:
                 np += p[i]
+    else:
+        np = p
     p = np
 
     max_w, max_p = len(s), len(p)
@@ -53,6 +67,3 @@ assert solution("aa", "*") == True
 assert solution("cb", "?a") == False
 assert solution("adceb", "*a*b") == True
 assert solution("acdcb", "a*c?b") == False
-
-solution('aaa', '***')
-
